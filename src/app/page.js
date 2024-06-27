569631ba425cd264b1ebc8db73763c25153e7d2c
ujstha <ujjawalshrestha7@gmail.com>
2024-06-27 05:20:15 +0300

    feat(components): Card component init; cn utils created

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import { Button } from "@/components";
 const Home = () => {
   return (
     <div className="absolute m-4 flex bg-red-400 p-5">
-      Homessalkfsjl
       <Button />
     </div>
   );
