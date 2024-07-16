e6fce8e732f2fd1e0d9e74c42c62db2bac5cbf2b
ujstha <ujjawalshrestha7@gmail.com>
2024-07-16 19:22:11 +0300

    feat(components): cssnano installed; Loader and Skeleton added

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 
 export const Providers = ({ children }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
--- src/app/trending/page.js
+++ src/app/trending/page.js
@@ -1,17 +1,14 @@
 "use client";
 
-import { CardHoverEffect, Loader, MediaCard, Section } from "@/components";
+import { CardHoverEffect, MediaCard, Section, Skeleton } from "@/components";
 import { useTrendings } from "@/hooks";
 
 const Trending = () => {
   const { isLoading, data } = useTrendings();
 
-  if (isLoading) {
-    return <Loader size="4" />;
-  }
-  console.log({ data });
   return (
     <Section containerClassName="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+      {isLoading && Array.from(Array(10), (v, i) => <Skeleton key={i} />)}
       {data?.results?.map((item, idx) => (
         <CardHoverEffect key={idx} index={idx}>
           <MediaCard item={item} />
